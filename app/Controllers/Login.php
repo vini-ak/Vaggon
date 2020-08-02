52d0a19f216fd03eac7e00f1e6021152bb1437ec
Vinícius <vinicius.vdes@gmail.com>
2020-08-02 17:47:59 -0300

    Fazendo tratamento do input dos usuários, verificando se o mesmo escolheu email e senhas válidas.

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -15,16 +15,47 @@ class Login extends BaseController
 
 		if ($this->request->getMethod() === 'post') {
 			$loginModel = new \App\Models\LoginModel();
-			$loginModel->set('user_email', $this->request->getPost('user_email'));
-			$loginModel->set('password', $this->request->getPost('password'));
+
+			$user_email = $this->request->getPost('user_email'); # EMAIL DO USUÁRIO
+			$pwd = $this->request->getPost('password'); # SENHA DO USUÁRIO
+
+			if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
+				// Verificando se o email do usuário é válido
+				$data['msg'] = "Oops... O formato de email está inválido.";
+			} 
+
+			else if (strlen($pwd) < 8) {
+				// Verificando se a senha tem 8 ou mais caracteres
+				$data['msg'] = "A senha precisa ter no mínimo 8 caracteres.";
+			}
 
 			
-			if ($loginModel->insert()) {
-				// a inserção ocorreu com sucesso
-				$data['msg'] = "Usuário cadastrado com sucesso";
-			} else {
-				$data['msg'] = "Erro ao cadastrar usuário.";
-			}
+			else {
+				// Verificando se o email já está cadastrado no banco de dados.
+				$db = \Config\Database::connect();
+
+				$sql = "SELECT id_user FROM users WHERE user_email = ?";
+				$query = $db->query($sql, [$user_email])->getResult();
+
+				// Caso não esteja ele será adicionado. 
+				// Se o email já estiver cadastrado, então será mostrada uma mensagem de erro.
+
+				if (!$query) {
+					$loginModel->set('user_email', $this->request->getPost('user_email'));
+					$loginModel->set('password', $this->request->getPost('password'));
+
+					if ($loginModel->insert()) {
+						// a inserção ocorreu com sucesso
+						$data['msg'] = "Usuário cadastrado com sucesso";
+					} else {
+						$data['msg'] = "Erro ao cadastrar usuário.";
+					}
+				}
+
+				else {
+					$data['msg'] = "O email já consta no nosso banco de dados.";
+				}
+			}			
 		}
 
 		echo View('signin_index', $data);
